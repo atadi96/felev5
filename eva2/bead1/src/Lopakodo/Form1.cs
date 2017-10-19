c45451d16232b9523527239aefb459c93847fcc1
Abonyi-Tóth Ádám <atadi96@gmail.com>
2017-10-19 18:07:59 +0200

    asd

--- eva2/bead1/src/Lopakodo/Form1.cs
+++ eva2/bead1/src/Lopakodo/Form1.cs
@@ -10,8 +10,22 @@ using System.Windows.Forms;
 
 namespace Lopakodo
 {
+    public class LopakodoViewEventArgs : EventArgs
+    {
+
+    }
+
+    public delegate void LopakodoViewEvent(object sender, LopakodoViewEventArgs args);
+
+    public interface ILopakodoViewState
+    {
+        LopakodoViewEvent Result { get; set; }
+    }
+
     public partial class Form1 : Form
     {
+        Stack<ILopakodoViewState> controls = new Stack<ILopakodoViewState>(5);
+
         public Form1()
         {
             InitializeComponent();
