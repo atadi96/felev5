190406e4cfdbe855157c060ae35cd86b7e9836c6
Abonyi-Tóth Ádám <abonyitothadam@INF.ELTE.HU>
2017-12-21 16:39:32 +0100

    Eva2|ZH|KA N KO RE

---
+++ eva2/zh/MemoryGame/MemoryGame.Model/Game.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MemoryGame.Model
+{
+    public class Game
+    {
+        public class Field
+        {
+            public int ID { get; private set; }
+            public bool Visible { get; set; }
+
+            public Field(int id)
+            {
+                ID = id;
+                Visible = false;
+            }
+        }
+
+        public class InteractionData
+        {
+            public int? ClickedID { get; set; } = null;
+        }
+
+        private Random random;
+
+        private readonly int size;
+        public int Size => size;
+
+        public int TargetFieldID { get; private set; }
+
+        private readonly Field[] fields;
+        public IReadOnlyCollection<Field> Fields => fields;
+
+        private readonly InteractionData interactionData;
+        public InteractionData Interactions => interactionData;
+
+        public EventHandler TickEvent;
+        private Timer tickTimer;
+
+        public Game(int size)
+        {
+            this.size = size;
+            random = new Random();
+            fields = CreateShuffled(size);
+            TargetFieldID = NewTargetFieldID();
+            interactionData = new InteractionData();
+            tickTimer = new Timer();
+            tickTimer.Interval = 2000;
+            tickTimer.Elapsed += (object o, ElapsedEventArgs e) =>
+            {
+                StepState();
+                TickEvent?.Invoke(this, null);
+            };
+            tickTimer.Start();
+        }
+
+        private IEnumerable<Field> InvisibleFields =>
+            fields.Where(field => field.Visible == false);
+
+        private int NewTargetFieldID()
+        {
+            var available = InvisibleFields.ToArray();
+            return available[random.Next(0, available.Length)].ID;
+        }
+
+        public void ClickID(int id)
+        {
+            if (interactionData.ClickedID != null) return;
+
+            Field clicked = fields.Where(f => f.ID == id && !f.Visible).FirstOrDefault();
+            if (clicked != null)
+            {
+                interactionData.ClickedID = clicked.ID;
+                clicked.Visible = true;
+            }
+        }
+
+        public void StepState()
+        {
+            if (TargetFieldID != interactionData.ClickedID && interactionData.ClickedID != null)
+            {
+                GetField(interactionData.ClickedID ?? -1).Visible = false;
+            }
+            if (InvisibleFields.ToArray().Length == 0)
+            {
+                tickTimer.Stop();
+                return;
+            }
+            TargetFieldID = NewTargetFieldID();
+            interactionData.ClickedID = null;
+        }
+
+        public Field GetField(int id)
+        {
+            return Fields.Where(field => field.ID == id).FirstOrDefault();
+        }
+
+        private Field[] CreateShuffled(int length)
+        {
+            var arr = Enumerable
+                        .Range(0, length)
+                        .Select(id => new Field(id))
+                        .ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int swapIndex = random.Next(i, length);
+                var temp = arr[i];
+                arr[i] = arr[swapIndex];
+                arr[swapIndex] = temp;
+            }
+            return arr;
+        }
+
+    }
+}
---
+++ eva2/zh/MemoryGame/MemoryGame.WPF/ViewModels/GameViewModel.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemoryGame.Model;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace MemoryGame.WPF.ViewModels
+{
+    class ViewCard
+    {
+        public int ID { get; set; }
+        public ImageSource Image { get; set; }
+
+        public ViewCard() { }
+
+        public ViewCard(int id, ImageSource img)
+        {
+            ID = id;
+            Image = img;
+        }
+    }
+
+    class GameViewModel : ViewModelBase
+    {
+        private Game game;
+
+        private BitmapImage cardImages;
+
+        private ICommand newGameCommand;
+        public ICommand NewGameCommand
+        {
+            get
+            {
+                if (newGameCommand == null)
+                {
+                    newGameCommand = new DelegateCommand(o => {
+                        int size = Convert.ToInt32((string)o);
+                        game = new Game(size);
+                        game.TickEvent += (object o2, EventArgs e) =>
+                            Application.Current.Dispatcher.Invoke(() => UpdateView());
+                        UpdateView();
+                    });
+                }
+                return newGameCommand;
+            }
+        }
+
+        private ICommand clickCommand;
+        public ICommand ClickCommand
+        {
+            get
+            {
+                if (clickCommand == null)
+                {
+                    clickCommand = new DelegateCommand(o => {
+                        int id = (int)o;
+                        game.ClickID(id);
+                        UpdateCards();
+                    });
+                }
+                return clickCommand;
+            }
+        }
+
+        private ObservableCollection<ViewCard> cards;
+        public ObservableCollection<ViewCard> Cards {
+            get
+            {
+                return cards;
+            }
+            private set
+            {
+                if (value != cards)
+                {
+                    cards = value;
+                    OnPropertyChanged("Cards");
+                }
+            }
+        }
+
+        public BitmapSource TargetImage { get; private set; }
+
+        public GameViewModel()
+        {
+            game = new Game(4);
+            game.TickEvent += (object o, EventArgs e) =>
+                Application.Current.Dispatcher.Invoke(() => UpdateView());
+            cardImages = new BitmapImage();
+            cardImages.BeginInit();
+            cardImages.UriSource = new Uri("Resources/cards.png", UriKind.Relative);
+            cardImages.EndInit();
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            TargetImage =
+                new CroppedBitmap(cardImages, IdToCrop(game.TargetFieldID));
+            OnPropertyChanged("TargetImage");
+            UpdateCards();
+        }
+
+        private Int32Rect IdToCrop(int id)
+        {
+            const int width = 200;
+            const int height = 274;
+            return new Int32Rect(id * width, 0, width, height);
+        }
+
+        private void UpdateCards()
+        {
+            var newCards =
+                game.Fields
+                    .Select(field => field.Visible
+                                ? new { id = field.ID, picID = field.ID }
+                                : new { id = field.ID, picID = 16 }
+                    ).Select(data => new ViewCard(
+                                        data.id,
+                                        new CroppedBitmap(
+                                            cardImages,
+                                            IdToCrop(data.picID)
+                                        )
+                    )).ToArray();
+            Cards = new ObservableCollection<ViewCard>(newCards);
+        }
+    }
+}
